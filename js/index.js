72001456ad215f0a7e80b1f43d9c836821a68d14
Humayun Kabir <kabirhumayun5902@gmail.com>
2022-12-28 03:21:37 +0600

    complete website desing

---
+++ js/index.js
@@ -0,0 +1,69 @@
+$('.slider').slick({
+    centerMode: true,
+    autoplay: true,
+    infinite: true,
+    centerPadding: '0',
+    slidesToShow: 1,
+});
+
+
+
+
+
+
+$('.slider-t').slick({
+    centerMode: true,
+    autoplay: true,
+    infinite: true,
+    centerPadding: '60px',
+    centerPadding: '0',
+    slidesToShow: 1,
+});
+
+
+$('.about-barand').slick({
+    infinite: true,
+    autoplay: true,
+    speed: 300,
+    centerPadding: '60px',
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [{
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 521,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
+
+
+
+
+$(".images img").click(function () {
+    $("#full-image").attr("src", $(this).attr("src"));
+    $('#image-viewer').show();
+});
+
+$("#image-viewer .close").click(function () {
+    $('#image-viewer').hide();
+});
